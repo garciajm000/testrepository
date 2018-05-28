63fcec04a97a7a93009d85c870967d3549afd0a5
JM Garcia <johnmichaelgarcia000@gmail.com>
2018-05-28 16:51:02 +0800

    updated form1.

--- test 2/test 2/Form1.cs
+++ test 2/test 2/Form1.cs
@@ -16,5 +16,10 @@ namespace test_2
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string x = "Hello World!";
+        }
     }
 }
